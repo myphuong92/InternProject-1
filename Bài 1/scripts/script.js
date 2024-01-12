36d0e1681f67418b4d95b302124e4f41a9285af9
Phương <20521779@gm.uit.edu.vn>
2024-01-12 14:55:36 +0700

    commit

--- Bài 1/scripts/script.js
+++ Bài 1/scripts/script.js
@@ -1,10 +1,13 @@
 // CAREER DETAILS PAGE
 const textarea = document.querySelector("textarea");
-textarea.addEventListener("keyup", (e) => {
-  textarea.style.height = "8rem";
-  let scHeight = e.target.scrollHeight;
-  textarea.style.height = `${scHeight}px`;
-});
+if (textarea) {
+  textarea.addEventListener("keyup", (e) => {
+    textarea.style.height = "8rem";
+    let scHeight = e.target.scrollHeight;
+    textarea.style.height = `${scHeight}px`;
+  });
+}
+
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
@@ -21,3 +24,21 @@ function hideSidebar() {
 //   // console.log(scHeight);
 //   contactTextarea.style.height = `${scHeight}px`;
 // });
+
+// HEADER RESPONSIVE
+const navbar = document.querySelector("header");
+const navHeight = navbar.offsetHeight;
+
+console.log("Nav height:", navHeight);
+let lastScrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    navbar.classList.add("nav-hidden");
+    // console.log("going down");
+  } else {
+    navbar.classList.remove("nav-hidden");
+
+    // console.log("going up");
+  }
+  lastScrollY = window.scrollY;
+});
